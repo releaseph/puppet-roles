d74d7a6a16f879de9c2bc9e33536bf5a8b02c91f
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-17 12:59:50 +0800

    enabled php and mysql

--- manifests/tng.pp
+++ manifests/tng.pp
@@ -1,8 +1,8 @@
 class role::tng {
 
     include ::profile::tng::apache
-    # include ::php
-    # include ::mysql::client
+    include ::php
+    include ::mysql::client
     # include ::profile::tng::admin
     # include ::profile::tng::go
 
