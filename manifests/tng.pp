b0bda050f0ae05f7fba9f37cfcea4aff27e7022c
Paolo Cadiz <pcadiz@cambridge.org>
2017-01-17 12:52:53 +0800

    enabled tng::apache

--- manifests/tng.pp
+++ manifests/tng.pp
@@ -1,6 +1,6 @@
 class role::tng {
 
-    # include ::profile::tng::apache
+    include ::profile::tng::apache
     # include ::php
     # include ::mysql::client
     # include ::profile::tng::admin
